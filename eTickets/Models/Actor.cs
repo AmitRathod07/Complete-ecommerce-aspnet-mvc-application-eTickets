599fd190bb80f4043aa5f5f8f7396fa16d7ab33d
AmitRathod07 <amit.rathod@outlook.in>
2023-07-10 18:38:26 +0530

    CRUD Operation in Actor Portion wtih validation.

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,12 +8,16 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture URL")]
+        [Required(ErrorMessage = "Profile Picture required!!!")]
         public string? ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name required!!!")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Full Name must be between 3 to 50 characters ")]
         public string? FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required!!")]
         public string? Bio { get; set; }
 
         //Relationship
