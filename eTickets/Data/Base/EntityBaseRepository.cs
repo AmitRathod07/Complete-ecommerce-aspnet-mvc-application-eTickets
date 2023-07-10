e2bd704fa728e103bcdd12f131eec71a241571b1
AmitRathod07 <amit.rathod@outlook.in>
2023-07-10 21:58:29 +0530

    Added addasync method

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
 {
@@ -12,11 +16,13 @@ namespace eTickets.Data.Base
             _context = context;
         }
 
-        public async Task AddAsync(T entity)
-        {
-            await _context.Set<T>().AddAsync(entity);
-            await _context.SaveChangesAsync();
-        }
+        //public async Task AddAsync(T entity)
+        //{
+        //    await _context.Set<T>().AddAsync(entity);
+        //    await _context.SaveChangesAsync();
+        //}
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
         public async Task DeleteAsync(int id)
         {
